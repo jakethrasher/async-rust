a513b0fbb1969dc6bf0c7ecaab3f1fad885f66bf
Jake Thrasher <mjakethrasher@gmail.com>
2024-06-19 17:40:48 -0700

    remove comments

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,6 @@ use sensible_api::Sensible;
 async fn main() {
     let sensible = Sensible::new(dotenv::var("TOKEN").unwrap());
 
-    // let file_path = "/Users/jakethrasher/Downloads/1_extract_your_first_data.pdf".to_string();
-    // let params = sensible_api::document::ExtractionParams {
-    //     file_path,
-    //     document_type: "senseml_basics".to_string(),
-    // };
-
-    // let response = sensible.document.async_extraction(params).await;
-
     let extraction_id = "39aa0399-519e-4088-b91d-57f2d7393a82".to_string();
 
     let response = sensible.document.retrieve_extraction(extraction_id).await;
